4c51b99d2e13b5fd6a5b2c98f8926bb6d01ea5ad
Ahmet Gökay Arpacı <ahmet.arpaci@MacBook-Pro-9.mynet>
2020-09-30 15:43:04 +0200

    fix report

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  document.documentElement.lang = "en";
   return (
     <>
       <Head>
